9f45cb7b636d612a3aa845dd7a2a74829a7e4857
carloscccu <99267965+carloscccu@users.noreply.github.com>
2023-12-06 19:39:09 +0000

    Ui bootstrap with angular

--- client/angular/angular-app/src/app/app.component.ts
+++ client/angular/angular-app/src/app/app.component.ts
@@ -1,14 +1,51 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+
+interface Item {
+  id: number;
+  userId: string;
+  lat: string;
+  lon: string;
+  description: string;
+  image: string;
+  keywords: string;
+}
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [CommonModule, FormsModule]
 })
 export class AppComponent {
-  title = 'angular-app';
+  items: Item[] = [];
+  tempItem: Item = this.getEmptyItem();
+
+  addItem(): void {
+    const newItem: Item = {
+      ...this.tempItem,
+      id: Date.now() // Using timestamp for unique ID
+    };
+    this.items.push(newItem);
+    this.tempItem = this.getEmptyItem(); // Reset form
+  }
+
+  deleteItem(itemId: number): void {
+    this.items = this.items.filter(item => item.id !== itemId);
+  }
+
+  private getEmptyItem(): Item {
+    return {
+      id: 0,
+      userId: '',
+      lat: '',
+      lon: '',
+      description: '',
+      image: '',
+      keywords: ''
+    };
+  }
 }
--- client/angular/angular-app/src/app/app.config.ts
+++ client/angular/angular-app/src/app/app.config.ts
@@ -1,8 +1,8 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    importProvidersFrom(FormsModule)
+  ]
 };
